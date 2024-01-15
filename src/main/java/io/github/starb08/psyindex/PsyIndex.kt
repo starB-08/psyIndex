67b25a2845718ff99183fe61f7201145eb850b25
Woohyun Seong <uve0508@naver.com>
2024-01-15 23:59:56 +0900

    Initial commit

---
+++ src/main/java/io/github/starb08/psyindex/PsyIndex.kt
@@ -0,0 +1,13 @@
+package io.github.starb08.psyindex
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PsyIndex : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
